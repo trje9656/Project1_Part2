3b881294b11e0115b17086afbc21d2885885d2f2
John-Henry-Wilson <70181657+John-Henry-Wilson@users.noreply.github.com>
2021-09-08 08:33:08 -0600

    Add files via upload

--- problem2/src/problem2/Range.java
+++ problem2/src/problem2/Range.java
@@ -2,56 +2,76 @@ package problem2;
 
 import java.util.*;
 import java.io.*;
-import java.util.Scanner;  
-import java.util.stream.IntStream;
 
 public class Range {
-	public static int[] extend(int n, int arr[], int num1, int num2) {
-		int i;
-		int newarr[] = new int[n + 10];
-		for (i = 0; i < n; i++)
-            newarr[i] = arr[i];
-		newarr = IntStream.rangeClosed(num1, num2).toArray();
-		return newarr;
+
+	String userInput;
+	int range1;
+	int range2;
+
+	public void read(){
+		//reads input
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your number range, seperated by a comma.");
+		userInput = input.next();
+
+		//seperates input by comma, stores into two ints
+		String[] ranges = userInput.split(",");
+		range1 = Integer.parseInt(ranges[0]);
+		range2 = Integer.parseInt(ranges[1]);
+
+		//if range is backwards or negative
+		if((range2 <= range1) || (range2 < 0) || (range1 < 0)){
+			System.out.println("Your range is incompatible. Please try again.");
+		}
+		//if termination condition
+		if(range1 == 0 && range2 == 0) {
+			System.exit(0);
+		}
+		//normally i'd call this through the main and use range.arraystuff but for the sake of the eternal loop i do it here
+		arrayStuff();
 	}
-	public static void calculate(int Array[],) {
-		System.out.print("Values :");
-		double sum, mean = 0;
-		for(int i = 0; i < Array.length; i++){
-			System.out.println(Array[i]);
-			sum += Array[i];
+
+	public void arrayStuff(){
+		//new array list woohoo dynamic shiz
+		ArrayList<Integer> arrayOfRange = new ArrayList<Integer>();
+		for(int i = 0; i < 10; i++){ // 100 times
+			for(int j = 0; j < 10; j++){ //increments of 10
+				Random r = new Random(); //random number
+				int num = r.nextInt((range2 - range1) + 1) + range1; //within the two ranges
+				arrayOfRange.add(num); //adds random number within ranges to array
+			}
+			printStuff(arrayOfRange); //beings printing
 		}
-		mean = sum/Array.length;
-		System.out.println("Mean :"+mean);
-		sum = 0;
-		for(int i = 0; i < Array.length; i++){
-			sum += Math.pow((Array[i]-mean),2);
+		//clearing out all the old stuff to make room for new ranges with the eternal loop
+		range1 = 0;
+		range2 = 0;
+		arrayOfRange.clear();
+		read();
+
+	}
+
+	public void printStuff(ArrayList<Integer> ranges){
+		System.out.println("");//formatting
+		System.out.print("Values:");
+		for(int i = 0; i < ranges.size(); i++){
+			System.out.print(" " + ranges.get(i)); //basic array iteration
 		}
-		mean=sum/(Array.length-1);
+		System.out.println("");
+
+		double counter, mean = 0;
+		for(int i = 0; i < ranges.size(); i++){
+			counter += ranges.get(i); //adds every value to a counter
+		}
+		mean = (counter / (double)(ranges.size())); //divides sum by size, formatted into double
+		System.out.println("Mean: " + mean); 
+		counter = 0;
+		//calculate standard deviation
+		for(int i = 0; i < ranges.size(); i++){
+			counter += Math.pow((ranges.get(i)-mean),2);
+		}
+		mean=counter/(ranges.size() - 1);
 		double deviation = Math.sqrt(mean);
 		System.out.println("SD :"+deviation);
+		
 	}
-	int [] Array = {};
-	while(Array.length < 100) 
-	{
-		Scanner in_num1 = new Scanner(System.in);
-		System.out.println("Enter Positive interger range value 1:");
-		int num1 = in_num1.nextInt();
-		Scanner in_num2 = new Scanner(System.in);
-		ystem.out.println("Enter Positive interger range value 2:");
-		int num2 = in_num2.nextInt();
-		if(num2 >= num1 | num1 < 0 | num2 < 0) 
-		{
-			System.out.println("INPUT INVALID");
-		}
-		else if(num1 == 0 & num2 == 0) 
-		{
-			break;
-		}
-		else 
-		{
-			Array = extend(Array.length, Array, num1, num2);
-			calculate(Array);
-		}
-	}
-}
