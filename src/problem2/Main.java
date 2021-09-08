61ddec8587acab0749cbcfeb7430e8c2524b9330
isbr3461 <isbr3461@colorado.edu>
2021-09-08 16:08:52 -0600

    added ioexceptions to everything , added file output

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -2,9 +2,10 @@ package problem2;
 
 import problem2.Range;
 
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) throws IOException {
 		problem2.Range Ranger= new problem2.Range();
 		Ranger.read();
 		
--- src/problem2/Range.java
+++ src/problem2/Range.java
@@ -2,14 +2,24 @@ package problem2;
 
 import java.util.*;
 import java.io.*;
-
+/* NOTE:
+YOU MUST TERMINATE THE PROGRAM BY ENTERING 0,0 TO MAKE EVERYTHING WRITE TO RESULTS2.TXT!!
+ */
 public class Range {
 
 	String userInput;
 	int range1;
 	int range2;
+	FileWriter out = new FileWriter("Results2.txt");
 
-	public void read() {
+	public Range() throws IOException {
+	}
+
+	public void Close() throws IOException{
+		out.close();
+	}
+
+	public void read() throws IOException {
 		//reads input
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter your number range, seperated by a comma.");
@@ -20,19 +30,23 @@ public class Range {
 		range1 = Integer.parseInt(ranges[0]);
 		range2 = Integer.parseInt(ranges[1]);
 
+
+		//if termination condition
+		if (range1 == 0 && range2 == 0) {
+			Close();
+			System.exit(0);
+		}
+
 		//if range is backwards or negative
 		if ((range2 <= range1) || (range2 < 0) || (range1 < 0)) {
 			System.out.println("Your range is incompatible. Please try again.");
 		}
-		//if termination condition
-		if (range1 == 0 && range2 == 0) {
-			System.exit(0);
-		}
+
 		//normally i'd call this through the main and use range.arraystuff but for the sake of the eternal loop i do it here
 		arrayStuff();
 	}
 
-	public void arrayStuff() {
+	public void arrayStuff() throws IOException {
 		//new array list woohoo dynamic shiz
 		ArrayList<Integer> arrayOfRange = new ArrayList<Integer>();
 		for (int i = 0; i < 10; i++) { // 100 times
@@ -51,20 +65,30 @@ public class Range {
 
 	}
 
-	public void printStuff(ArrayList<Integer> ranges) {
-		System.out.println("");//formatting
+	public void printStuff(ArrayList<Integer> ranges)throws IOException {
+		 //opens writer
+		System.out.print("\n");//formatting
+		out.write("\n");
+
 		System.out.print("Values:");
+		out.write("Values:");
+
 		for (int i = 0; i < ranges.size(); i++) {
 			System.out.print(" " + ranges.get(i)); //basic array iteration
+			out.write(" " + ranges.get(i));
 		}
 		System.out.println("");
+		out.write("\n");
 
 		double counter = 0, mean = 0;
 		for (int i = 0; i < ranges.size(); i++) {
 			counter += ranges.get(i); //adds every value to a counter
 		}
 		mean = (counter / (double) (ranges.size())); //divides sum by size, formatted into double
+
 		System.out.println("Mean: " + mean);
+		out.write("Mean: " + mean + "\n");
+
 		counter = 0;
 		//calculate standard deviation
 		for (int i = 0; i < ranges.size(); i++) {
@@ -73,6 +97,7 @@ public class Range {
 		mean = counter / (ranges.size() - 1);
 		double deviation = Math.sqrt(mean);
 		System.out.println("SD :" + deviation);
+		out.write("SD: " + deviation + "\n");
 
 	}
 }
